83f00b0a327a811ad29121277662c8f261709d6a
ChicagoScanningMG <77636200+ChicagoScanningMG@users.noreply.github.com>
2021-10-01 17:14:50 -0500

    Updating With new Script to Receive info from GS
    Last update accidentally replaced the script with the CSS file.

--- script.js
+++ script.js
@@ -1,139 +1,197 @@
-/*
- * Globals
- */
+var spinner = $('#loader');
+const scriptURLC ='https://script.google.com/macros/s/AKfycbw8vmnJGMA1KxM0dS6cgPDsipCjRTT5qhLoNLm1kWePd67oAVeC5XDHRpWL2rcc6mdO/exec';
+// const scriptURLC ='turd';
+const serverlessForm = document.forms['serverless-form'];
+
+$("#reset-btn").click(function(){
+  $("#contactForm").trigger("reset");
+  $(".pulled-detail").html("");
+});
+
+$("#order-id").change(function(){
+
+  var orderID = $('#order-id').val();
+  var tapeName = orderID.split("_");
+
+  if (tapeName.length > 1){
+
+    $('#order-id').val(tapeName[0]);
+    $('#last-name').val(tapeName[1]);
+    $('#tape-num').val(tapeName[2]);
+    $('#tape-num').focus();
+
+  }
 
 
-/* Custom default button */
-.btn-secondary,
-.btn-secondary:hover,
-.btn-secondary:focus {
-  color: #333;
-  text-shadow: none; /* Prevent inheritance from `body` */
+
+})
+
+$('#order-id').change(function(){
+  $('#order-notes').html("Retrieving Order Notes");
+  $('#last-name').attr("placeholder", "Retrieving Customer Name");
+  getOrderDetails($('#order-id').val(), $('#tape-num').val());
+})
+
+$('#tape-num').change(function(){
+  $('#existing-qc-notes').html("Retrieving Tape Notes");
+  getOrderDetails($('#order-id').val(), $('#tape-num').val());
+})
+
+
+
+
+
+serverlessForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    var orderID = $('#order-id').val();
+    var lastName = $('#last-name').val();
+    var tapeNum = $('#tape-num').val();
+    var qcNotes = $('#qc-notes').val();
+    var billingNotes = $('#billing-notes').val();
+
+    Swal.fire({
+      title: 'Ready to Log Tape <br/>' + orderID + '_' + lastName + '_ ' + tapeNum + '?',
+      text: 'Has the file been reviewed?  Ready to Log Tape?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Log Tape',
+      returnFocus: false,
+      focusCancel: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Swal.fire('Saved!', '', 'success')
+        logTape(e)
+      }
+    })
+
+});
+
+function logTape(e){
+
+  spinner.show();
+
+  var orderID = $('#order-id').val();
+  var tapeNum = $('#tape-num').val();
+  var qcNotes = $('#qc-notes').val();
+  var billingNotes = $('#billing-notes').val();
+
+  console.log(orderID);
+
+  var tapeProgress = "";
+
+  fetch(scriptURLC, {
+          method: 'POST',
+          body: new FormData(serverlessForm)
+      })
+      .then(res => {
+
+          console.log(res);
+          spinner.hide();
+
+          res.json().then(function(data) {
+            console.log(data);
+            tapeProgress = data.tapeProgress;
+
+            if (res['status'] == 200) {
+
+                if(data.errorTitle == ""){
+                  Swal.fire({
+                    title: "Tape Logged!",
+                    html: "Tape #" + tapeNum + " For Order " + orderID + "_" + $("#last-name").val() + " Has Been Logged!" + "<br/>" + data.tapeProgress,
+                    icon: "success",
+                    returnFocus: false
+                  }).then((result) => {
+                    if (result.isConfirmed) {
+                      // Swal.fire('Saved!', '', 'success')
+                      // $(".resetAble").val("");
+                      $('#order-id').val(orderID);
+                      $('#stop-time').val("")
+                      $('#tape-num').val("");
+                      $('#qc-notes').val("");
+                      $("#billing-notes").val("").change();
+                      $('#needs-review').prop('checked', false);
+                      $('#notes-only').prop('checked', false);
+                      $('#existing-qc-notes').html("");
+                      $('#tape-num').focus();
+                    }
+                  });
+                } else { //There was a logging error
+
+                  Swal.fire({
+                    title: data.errorTitle,
+                    html: data.errorText,
+                    icon: "error",
+                    returnFocus: false
+                  });
+
+                }
+
+                return true;
+
+            } else {
+                Swal.fire("Something went wrong!", "Review Footage Log and/or contact Admin<br/>" + orderID + "<br/>" + tapeNum + "<br/>" + qcNotes + "<br/>" + billingNotes, "error");
+
+            }
+
+          });
+
+
+
+          document.getElementById('submitForm').classList.remove('loading');
+
+      })
+      .catch(error => {
+          spinner.hide();
+          // document.getElementById('submitForm').classList.remove('loading');
+          Swal.fire("Something went wrong!", "Review Footage Log and/or contact Admin", "error");
+          // todo enable submit button
+
+      })
 }
 
+function getOrderDetails(orderID, tapeNum){
 
-/*
- * Base structure
- */
+  const params = new URLSearchParams({
+    orderID: orderID,
+    tapeNum: tapeNum,
+    getOrderDetails: true
+  });
 
-body {
-  text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
-  box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
-	background-color: black;
-  color: white;
-  margin: 5px;
-}
+    fetch(scriptURLC, {
 
-main{
-    border: 2em double;
-    border-color: white;
-}
+      method: 'POST',
+      body: params
 
-h3 {
+    }).then(response => response.json())
+    .then(data => {
 
-  font-size: 0.8em;
+      $("#existing-qc-notes").html(data.tapeNotes);
+      $("#order-notes").html(data.orderNotes);
+      $("#last-name").val(data.customerName);
 
-}
+      copyToClipboard($('#order-id').val() + "_" + $('#last-name').val() + "_" + ('000' + $('#tape-num').val()).slice(-3))
 
-.cover-container {
-  max-width: 42em;
-}
+      $('#last-name').attr("placeholder", "Smith");
 
-input {
+      if ($('#tape-num').val() == "") {
+        $('#tape-num').focus();
+      } else if ($("#initials").val() == "") {
+        $('#initials').focus();
+      } else {
+        $('#qc-notes').focus();
+      }
 
-  border-radius: 5px;
-  border-style: none;
-  /* margin: 5px; */
-
-}
-
-button {
-
-  border-radius: 100px;
-  border-style: none;
-  /* margin: 5px; */
+      console.log(data)
+    });
 
 }
 
-label {
-
-    text-align: right;
-    font-size: 0.8em;
-
-}
-
-.row {
-
-  margin: 5px;
-
-}
-
-.video-notes-field {
-
-  font-size: .8em;
-
-}
-
-textarea {
-
-  border-radius: 5px;
-  border-style: none;
-
-}
-
-.pulled-detail{
-  
-  margin: 0;
-
-}
-
-
-/*
- * Header
- */
-
-.nav-masthead .nav-link {
-  padding: .25rem 0;
-  font-weight: 700;
-  color: rgba(255, 255, 255, .5);
-  background-color: transparent;
-  border-bottom: .25rem solid transparent;
-}
-
-.nav-masthead .nav-link:hover,
-.nav-masthead .nav-link:focus {
-  border-bottom-color: rgba(255, 255, 255, .25);
-}
-
-.nav-masthead .nav-link + .nav-link {
-  margin-left: 1rem;
-}
-
-.nav-masthead .active {
-  color: #fff;
-  border-bottom-color: #fff;
-}
-
-/*
- *Link Styles
- */
-.white-link{
-	color: white;
-}
-
-.bg-pink{
-  background-color: rgb(230, 3, 214);
-}
-
-.text-white{
-  color: white;
-}
-
-.single-line-text{
-  margin: 5px;
-}
-
-.move-left {
-    width: auto;
-    box-shadow: none;
+function copyToClipboard(value) {
+  var tempInput = document.createElement("input");
+  tempInput.value = value;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
 }
