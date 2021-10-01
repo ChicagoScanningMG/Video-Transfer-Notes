98e7513d0a3b5a391ef0fa99fdbb83838f955cd8
ChicagoScanningMG <77636200+ChicagoScanningMG@users.noreply.github.com>
2021-10-01 17:14:12 -0500

    Updated for receiving info from GS

--- script.js
+++ script.js
@@ -1,113 +1,139 @@
-var spinner = $('#loader');
-const scriptURLC ='https://script.google.com/macros/s/AKfycbzpkY2xyES0EpG_lWCa8AXWiIsWIYOWUGfB1e6re81KZ7xnhIdMog7DpOVAJzz1ME3Y/exec';
-// const scriptURLC ='turd';
-const serverlessForm = document.forms['serverless-form'];
-
-$("#reset-btn").click(function(){
-  $("#contactForm").trigger("reset");
-});
-
-$("#order-id").change(function(){
-
-  var orderID = $('#order-id').val();
-  var tapeName = orderID.split("_");
-
-  if (tapeName.length > 1){
-
-    $('#order-id').val(tapeName[0]);
-    $('#last-name').val(tapeName[1]);
-    $('#tape-num').val(tapeName[2]);
-    $('#tape-num').focus();
-
-  }
-
-})
+/*
+ * Globals
+ */
 
 
-
-serverlessForm.addEventListener('submit', e => {
-    e.preventDefault();
-
-    var orderID = $('#order-id').val();
-    var lastName = $('#last-name').val();
-    var tapeNum = $('#tape-num').val();
-    var qcNotes = $('#qc-notes').val();
-    var billingNotes = $('#billing-notes').val();
-
-    Swal.fire({
-      title: 'Ready to Log Tape <br/>' + orderID + '_' + lastName + '_ ' + tapeNum + '?',
-      text: 'Has the file been reviewed?  Ready to Log Tape?',
-      icon: 'question',
-      showCancelButton: true,
-      confirmButtonText: 'Log Tape',
-      returnFocus: false,
-      focusCancel: true
-    }).then((result) => {
-      if (result.isConfirmed) {
-        // Swal.fire('Saved!', '', 'success')
-        logTape(e)
-      }
-    })
-
-});
-
-function logTape(e){
-
-  spinner.show();
-
-  var orderID = $('#order-id').val();
-  var tapeNum = $('#tape-num').val();
-  var qcNotes = $('#qc-notes').val();
-  var billingNotes = $('#billing-notes').val();
-
-  console.log(orderID);
-
-  fetch(scriptURLC, {
-          method: 'POST',
-          body: new FormData(serverlessForm)
-      })
-      .then(res => {
-
-          console.log(res);
-          spinner.hide();
-
-          if (res['status'] == 200) {
-              Swal.fire({
-                title: "Tape Logged!",
-                text: "Tape #" + tapeNum + " For Order " + orderID + " Has Been Logged!",
-                icon: "success",
-                returnFocus: false
-              }).then((result) => {
-                if (result.isConfirmed) {
-                  // Swal.fire('Saved!', '', 'success')
-                  $('#tape-num').focus();
-                }
-              });
-
-              // $(".resetAble").val("");
-              $('#order-id').val(orderID);
-              $('#stop-time').val("")
-              $('#tape-num').val("");
-              $('#qc-notes').val("");
-              $("#billing-notes").val("").change();
-              $('#needs-review').prop('checked', false);
-              $('#notes-only').prop('checked', false);
-
-              return true;
-
-          } else {
-              Swal.fire("Something went wrong!", "Review Footage Log and/or contact Admin<br/>" + orderID + "<br/>" + tapeNum + "<br/>" + qcNotes + "<br/>" + billingNotes, "error");
-
-          }
-          document.getElementById('submitForm').classList.remove('loading');
-
-      })
-      .catch(error => {
-          spinner.hide();
-          Swal.fire("Something went wrong!", "Review Footage Log and/or contact Admin", "error");
-          // todo enable submit button
-
-      })
+/* Custom default button */
+.btn-secondary,
+.btn-secondary:hover,
+.btn-secondary:focus {
+  color: #333;
+  text-shadow: none; /* Prevent inheritance from `body` */
+}
 
 
+/*
+ * Base structure
+ */
+
+body {
+  text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+  box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
+	background-color: black;
+  color: white;
+  margin: 5px;
+}
+
+main{
+    border: 2em double;
+    border-color: white;
+}
+
+h3 {
+
+  font-size: 0.8em;
+
+}
+
+.cover-container {
+  max-width: 42em;
+}
+
+input {
+
+  border-radius: 5px;
+  border-style: none;
+  /* margin: 5px; */
+
+}
+
+button {
+
+  border-radius: 100px;
+  border-style: none;
+  /* margin: 5px; */
+
+}
+
+label {
+
+    text-align: right;
+    font-size: 0.8em;
+
+}
+
+.row {
+
+  margin: 5px;
+
+}
+
+.video-notes-field {
+
+  font-size: .8em;
+
+}
+
+textarea {
+
+  border-radius: 5px;
+  border-style: none;
+
+}
+
+.pulled-detail{
+  
+  margin: 0;
+
+}
+
+
+/*
+ * Header
+ */
+
+.nav-masthead .nav-link {
+  padding: .25rem 0;
+  font-weight: 700;
+  color: rgba(255, 255, 255, .5);
+  background-color: transparent;
+  border-bottom: .25rem solid transparent;
+}
+
+.nav-masthead .nav-link:hover,
+.nav-masthead .nav-link:focus {
+  border-bottom-color: rgba(255, 255, 255, .25);
+}
+
+.nav-masthead .nav-link + .nav-link {
+  margin-left: 1rem;
+}
+
+.nav-masthead .active {
+  color: #fff;
+  border-bottom-color: #fff;
+}
+
+/*
+ *Link Styles
+ */
+.white-link{
+	color: white;
+}
+
+.bg-pink{
+  background-color: rgb(230, 3, 214);
+}
+
+.text-white{
+  color: white;
+}
+
+.single-line-text{
+  margin: 5px;
+}
+
+.move-left {
+    width: auto;
+    box-shadow: none;
 }
