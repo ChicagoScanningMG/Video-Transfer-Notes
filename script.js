9769f85abd0e9fdec3c5b87acf0f4486acb9adeb
ChicagoScanningMG <77636200+ChicagoScanningMG@users.noreply.github.com>
2021-10-15 13:24:44 -0500

    Added a timer

--- script.js
+++ script.js
@@ -2,11 +2,118 @@ var spinner = $('#loader');
 const scriptURLC ='https://script.google.com/macros/s/AKfycbwOfIsJmBwuIP4LF633tpTgrR7GJD9M8b4NN0EZ7fmSueHKXXcr0gMgeCnNzW7n7GFv/exec';
 // const scriptURLC ='turd';
 const serverlessForm = document.forms['serverless-form'];
+var timerOn = false;
+var bgAnimate = false;
 
 $("#reset-btn").click(function(){
   $("#contactForm").trigger("reset");
   $(".pulled-detail").html("");
+  resetTimer();
 });
+
+var countdownDuration = 10 * 60 * 1000;
+
+$("#restart-button").click(function(){
+
+  console.log("restart button Clicked");
+  countdownDuration = 10 * 60 * 1000;
+  $("#stop-timer").html("10:00");
+  if (timerOn == false){
+
+    timerOn = true;
+    runTimer();
+
+  }
+  bgAnimate = false;
+
+})
+
+$("#stop-button").click(function(){
+  console.log("stop button Clicked");
+  resetTimer();
+
+})
+
+function resetTimer(){
+
+  timerOn = false;
+  bgAnimate = false;
+  $("#stop-timer-button").css("display", "inherit");
+  $("#stop-timer-div").css("display", "none");
+  $("body").css("background-color", "black");
+  // clearInterval(timer);
+  countdownDuration = 10 * 60 * 1000;
+
+}
+
+$("#stop-timer-button").click(function(){
+
+  runTimer();
+
+})
+
+function runTimer(){
+
+  console.log("stop timer button clicked");
+  timerOn = true;
+  $("#stop-timer-button").css("display", "none");
+  $("#stop-timer-div").css("display", "inherit");
+  $("#stop-timer").css("width", $("#stop-timer").width());
+
+
+  var now = new Date().getTime();
+  var countdownTime = now + countdownDuration;
+
+  console.log("Countdown Duration: " + countdownDuration);
+  console.log("current Time: " + now);
+  console.log("Countdown Time: " + countdownTime);
+
+  var timer = setInterval(function(){
+    console.log("Tick");
+
+    var now = new Date().getTime();
+
+    var distance = (now + countdownDuration) - now;
+    console.log("countdownTime: " + countdownTime);
+
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (seconds < 10) seconds = "0" + seconds;
+
+    $("#stop-timer").html(minutes + ":" + seconds);
+
+    countdownDuration = countdownDuration - 1000;
+
+    // If the count down is finished, write some text
+    if (distance <= 0) {
+      clearInterval(timer);
+      countdownDuration = 10 * 60 * 1000;
+      timerOn = false;
+      bgAnimate = true;
+
+      var flashRed = setInterval(function(){
+
+        if (bgAnimate == false){
+          clearInterval(flashRed);
+          return;
+        }
+
+        $("body").animate({"background-color": "rgb(235, 0, 0)"}, 400)
+          .delay(400)
+          .animate({"background-color": "black"}, 400);
+
+      }, 1600);
+
+    }
+
+    if (timerOn == false) {
+      clearInterval(timer);
+      countdownDuration = 10 * 60 * 1000;
+    }
+
+  }, 1000);
+
+}
 
 $("#order-id").change(function(){
 
@@ -106,6 +213,7 @@ function logTape(e){
                     if (result.isConfirmed) {
                       // Swal.fire('Saved!', '', 'success')
                       // $(".resetAble").val("");
+                      resetTimer();
                       $('#order-id').val(orderID);
                       $('#stop-time').val("")
                       $('#tape-num').val("");
